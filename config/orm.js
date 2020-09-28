cad4a741c65cb629d1688db35dc73ae781ebb411
Matari Howard <howardmatari@gmail.com>
2020-09-27 23:37:21 -0400

    Fixed connection

--- config/orm.js
+++ config/orm.js
@@ -1,96 +1,77 @@
-//const connection = require ("./connection")
-
-
-// Import MySQL connection.
 var connection = require("../config/connection.js");
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
-function printQuestionMarks(num) {
+// Import MySQL connection.
+function createQmarks(num){
   var arr = [];
+
   for (var i = 0; i < num; i++) {
     arr.push("?");
   }
+
   return arr.toString();
-}
-// Helper function to convert object key/value pairs to SQL syntax
-function objToSql(ob) {
+};
+
+function translateSQL(obj){
   var arr = [];
+
   // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
     // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      // if string with spaces, add quotations 
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
+      // e.g. {devouer: true} => ["devouer=true"]
       arr.push(key + "=" + value);
     }
   }
+
   // translate array of strings to a single comma-separated string
   return arr.toString();
 }
-// Object for all our SQL statement functions.
-var orm = {
-  all: function(tableInput, cb) {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
-      if (err) {
+
+
+var orm ={
+  selectAll: function(table,cb){
+    var dbQuery = "SELECT * FROM " + table + ";";
+    connection.query(dbQuery, function(err,res){
+      if(err){
         throw err;
       }
-      cb(result);
+      cb(res);
     });
   },
-  create: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
-    console.log(queryString);
-    connection.query(queryString, vals, function(err, result) {
-      if (err) {
+  insertOne:function(table,cols,vals,cb){
+    var dbQuery = "INSERT INTO " + table +"(" + cols.tostring()+")"+ "VALUES ("+ createQmarks(vals.length)+")";
+  console.log(dbQuery);
+  connection.query(dbQuery,vals,function(err, res){
+    if(err){
+      throw err;
+    }
+    cb(res);
+  });
+  
+  },
+  insertOne:function(table,objColVals,condtion,cb){
+    var dbQuery= "UPDATE" + table + "SET" + translateSQL(objColVals)+"WHERE"+ condtion;
+    connection.query(dbQuery,vals,function(err, res){
+      if(err){
         throw err;
       }
-      cb(result);
+      cb(res);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
+  deleteOne: function(table,condtion,cb){
+    var dbQuery= "DELETE FROM" + table + "WHERE" + condtion;
+    connection.query(dbQuery,vals,function(err, res){
+      if(err){
         throw err;
       }
-      cb(result);
+      cb(res);
     });
   }
-};
+}
+
 // Export the orm object for the model (cat.js).
 module.exports = orm;
-
-//  const orm = {
-//      selectAll: function(){
-//          connection.query("SELECT * FROM Burgers", (err,result)=>{})
-//      },
-//     insertOne: function(){
-
-//     },
-//      updateOne: function(){
-
-//      },
-//  }
-
-  //  module.exports = orm
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,42 +1,57 @@
-var express = require("express");
+//router connections file
 
-var router = express.Router();
+const express = require("express");
+const connection = require("../config/connection.js");
+// Import the model (burger.js) to use its database functions.
+const burger = require("../models/burger.js");
 
-var burger = require("../models/burger.js");
+const router = express.Router();
 
-//Create all our routes and set up logic within those routes where required.
-router.get("/", function(req,res) {
-    burger.all(function(data) {
-        var hbsObject = {
-            burgers: Data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
-    });
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
 
-router.post('/api/burgers', function(req, res){
-    burger.insertOne(req.body.burger_name, req.body.devoured, function(result) {
-        res.json({id: result.insertId});
-    });
+router.post('/api/burgers', function (req, res) {
+  burger.insertOne(req.body.burger_name, req.body.devoured, function (result) {
+    res.json({ id: result.insertId });
+  });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
 
-    console.log("condition", condition);
+router.put("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
 
-    burger.updateone({
-        devoured: req.body.devoured
-    }, condition, function(result) {
-        if (result.changedRows==0) {
-            //If no rows were change, then Id must not exists, so 404
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function (result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+  router.deleteOne(condition, function (req, res) {
+    var condition = " id = " + req.params.id;
+    console.log("condition", condition, function (result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
-
+      } else {
+        res.status(200).end();
+      }
+    })
+  })
 });
-//Export routes for server.js to use
+
+// Export routes for server.js to use.
 module.exports = router;
